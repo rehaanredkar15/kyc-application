641e608c3bff5ebc5a38fa82826f8d4d8e201c62
rehaanredkar15 <rehaanredkar92@gmail.com>
2024-12-12 20:39:55 +0530

    fix - Added the protection on routes for role based access

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import KYCFormPage from "./pages/KYCFormPage";
@@ -6,18 +6,49 @@ import DashboardPage from "./pages/DashboardPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-
+  const ProtectedRoute = ({ children, allowNonAdmin }) => {
+    const user = JSON.parse(localStorage.getItem("user"));
+  
+    if (!user) {
+      return <Navigate to="/" replace />;
+    }
+  
+    if (allowNonAdmin) {
+      return children;
+    }
+      if (user.role !== "admin") {
+      return <Navigate to="/kyc" replace />; 
+    }
+      return children;
+  };
   return (
     <>
      <Router>
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/kyc" element={<KYCFormPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+          
+          <Route 
+            path="/kyc" 
+            element={
+              <ProtectedRoute allowNonAdmin={true}>
+                <KYCFormPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route 
+            path="/dashboard" 
+            element={
+              <ProtectedRoute allowNonAdmin={false}>
+                <DashboardPage />
+              </ProtectedRoute>
+            } 
+          />
+
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
-      </Router> 
+      </Router>
     </>
   )
 }
