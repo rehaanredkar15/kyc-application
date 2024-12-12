75b15f4eb80644e79960284d1435666b627d16a5
rehaanredkar15 <rehaanredkar92@gmail.com>
2024-12-12 18:42:24 +0530

    Fix - Added the authentication token for the backend requests

--- src/network/kyc-api.js
+++ src/network/kyc-api.js
@@ -2,4 +2,47 @@ import axios from 'axios';
 
 const KycAPI = axios.create({ baseURL: 'http://localhost:5500/v1/'});
 
-export const login = (orderDetails) => KycAPI.post(`/auth/login`, orderDetails);
+const getAccessToken = async () => {
+    const useremail = JSON.parse(localStorage.getItem('useremail')) || [];
+
+    const authBody = {
+      email: useremail,
+    };
+    return KycAPI.post('/auth/refresh-token', authBody).then(response => {
+      localStorage.setItem("token", JSON.stringify(response.data.token));
+    });
+};
+
+KycAPI.interceptors.request.use(async (req) => {
+    if (localStorage.getItem('token')) {
+      const token  = localStorage.getItem('token');
+      if (token) {
+          req.headers.Authorization = `${token}`;
+      }
+    }
+    return req;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+KycAPI.interceptors.response.use(response => {
+    return response;
+  }, error => {
+    if (error.response && error.response.status === 401) {
+         return getAccessToken().then(() => {
+             const config = error.config;
+             const token = JSON.parse(localStorage.getItem('token'));    
+             config.headers['Authorization'] = `Bearer ${token}`;
+             return axios.request(config);
+            });
+    }
+    return Promise.reject(error);
+  });
+
+export const login = (logindetails) => KycAPI.post(`/auth/login`, logindetails);
+
+export const register = (userDetails) => KycAPI.post(`/auth/register`, userDetails);
+
+export const uploadkyc = (userDetails) => KycAPI.post(`/users/upload-kyc`, userDetails);
